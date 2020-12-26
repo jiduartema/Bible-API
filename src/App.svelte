a7ad630081d56a0c0b89edcbcbb2dc4a33f058aa
Jorge Duarte Marin <jorgeivanduartemarin6b@gmail.com>
2020-12-26 11:42:11 -0500

    Delete devotionals

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,35 @@
 <script>
-    import {db} from './firebase';
+    import { db } from "./firebase";
 
     let Devotional = {
         biblicalQuotes: "",
-        description: ""
-    }
+        description: "",
+    };
+    let listDevotional = [];
 
-    const addDevotional = () => {
-        db.collection('devotionals').doc().set(Devotional);
+    db.collection('devotionals').onSnapshot(querySnapshot => {
+        let docs = [];
+        querySnapshot.forEach(doc => {
+            docs.push({...doc.data(), id: doc.id});
+        })
+        listDevotional = [...docs];
+    });
+
+    const addDevotional = async () => {
+        await db.collection("devotionals").doc().set({
+            ...Devotional, createdAt_: Date.now()
+        });
+        console.log('New Devotional created');
     };
 
     const sendDevotional = (e) => {
         e.preventDefault();
         addDevotional();
+        Devotional = {biblicalQuotes: '', description: ''};
+    };
+    const deleteDevotional = async(id) => {
+        await db.collection('devotionals').doc(id).delete();
+        console.log('Devotional deleted successfully!');
     }
 </script>
 
@@ -20,7 +37,23 @@
 </style>
 
 <form on:submit={sendDevotional}>
-    <input bind:value={Devotional.biblicalQuotes} type="text" placeholder="Biblical quotes" />
-    <textarea bind:value={Devotional.description} rows="3" placeholder="Description" />
+    <input
+        bind:value={Devotional.biblicalQuotes}
+        type="text"
+        placeholder="Biblical quotes" />
+    <textarea
+        bind:value={Devotional.description}
+        rows="3"
+        placeholder="Description" />
     <button> save </button>
 </form>
+
+{#each listDevotional as listDevotionals}
+<div>
+    <h5>{listDevotionals.biblicalQuotes}</h5>
+    <p>{listDevotionals.description}</p>
+    <button on:click={deleteDevotional(listDevotionals.id)}>
+        delete
+    </button>
+</div>
+{/each}
